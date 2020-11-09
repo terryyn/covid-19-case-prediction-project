3e5fd4bc377b767d4ce8c6b4a90246f34d03841c
Terry Ye <yeyunong@hotmail.com>
2020-11-09 00:23:08 -0800

    add argument to test

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np 
+import sys
 
 test_dir = "C:\\Users\\yeyun\\Google Drive\\university\\2020 fall\\CS 145\\covid-19-case-prediction-project\\data\\"
 test_file = "sept_final.csv"
 predict_dir = "C:\\Users\\yeyun\\Google Drive\\university\\2020 fall\\CS 145\\covid-19-case-prediction-project\\"
-predict_file = "ar_submission.csv"
+
 
 def mean_absolute_percentage_error(y_true, y_pred): 
     """
@@ -14,6 +15,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 
 def main():
+    predict_file = sys.argv[1]
     test_data = np.genfromtxt(test_dir + test_file, dtype=int, delimiter=',', names=True)
     predict_data = np.genfromtxt(predict_dir + predict_file, dtype=int, delimiter=',', names=True)
     confirmed_mape = mean_absolute_percentage_error(test_data['Confirmed'], predict_data['Confirmed'])
